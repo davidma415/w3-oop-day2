056837b8eb55ff5efa576c1010ec7cd2da70c786
David Ma <davidma415@gmail.com>
2018-07-22 13:57:32 -0400

    Finished zombies

--- zombie.rb
+++ zombie.rb
@@ -6,6 +6,50 @@ class Zombie
   @@default_speed = 1
   @@default_strength = 3
 
+  def initialize(spd, str)
+    if spd > @@max_speed
+      @spd = @@default_speed
+    else
+      @spd = spd
+    end
+
+    if str > @@max_strength
+      @str = default_strength
+    else
+      @str = str
+    end
+  end
+
+  def outrun_zombie?
+    run_speed = rand(@@max_speed)
+    if run_speed > @spd
+      return true
+    else
+      return false
+    end
+  end
+
+  def survive_attack?
+    random_str = rand(@@max_strength)
+    if random_str > @str
+      return true
+    else
+      return false
+    end
+  end
+
+  def encounter
+    if outrun_zombie? == true
+      return "You outran the zombie!"
+    elsif outrun_zombie? == false && survive_attack? == true
+      @@horde << initialize(rand(@@max_speed), rand(@@max_strength))
+      return "You are now a zombie!"
+    elsif outrun_zombie? == false && survive_attack? == false
+      return "You have died."
+    else
+      return "You outran the zombie!"
+    end
+  end
   def self.plague_level
     @@plague_level
   end
@@ -14,21 +58,42 @@ class Zombie
     @@plague_level = int
   end
 
-  def initialize(spd, str)
-    if spd > @@max_speed
-      spd = @@default_speed
-    else
-      spd = spd
-    end
+  def self.all
+    return @@horde
+  end
 
-    if str > @@max_strength
-      str = default_strength
-    else
-      str = str
+  def self.spawn
+    rand(@@plague_level).times do
+      @@horde << self.new(rand(@@max_speed), rand(@@max_strength))
     end
   end
 
+  def self.increase_plague_level
+    @@plague_level += rand(0..3)
+  end
+
+  def self.some_die_off
+    rand(0..11).times do
+      @@horde.delete(@@horde.sample)
+    end
+  end
+
+  def self.new_day
+    self.some_die_off
+    self.spawn
+    self.increase_plague_level
+  end
 end
 
 carl = Zombie.new(3, 3)
-p carl
+Zombie.spawn
+puts Zombie.all
+puts "It's a new day"
+Zombie.new_day
+puts Zombie.all
+puts Zombie.plague_level
+
+
+zombie1 = Zombie.all[0]
+
+puts zombie1.encounter
